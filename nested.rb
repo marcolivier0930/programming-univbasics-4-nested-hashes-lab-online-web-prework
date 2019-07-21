c55c56210c53b1f204ad6304cdd93f25a8d9e530
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-21 18:18:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages.keys.join]
+     languages = programmer_hash[:dennis_ritchie][:languages]
+     languages.to_s 
 end
 
 def adding_matz
